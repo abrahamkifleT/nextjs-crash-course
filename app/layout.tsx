de19597760b22ca54537f01ed633ab43273d5fec
abraham <abrahamkifle1212@gmail.com>
2026-01-03 20:33:25 +0300

    feat: Initialize Next.js application with global styles, main page, event components, and a light ray effect.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from "next";
 import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
 import "./globals.css";
 import LightRays from "@/components/LightRays";
+import Navbar from "@/components/Navbar";
 
-const schibsteg_Grotesk = Schibsted_Grotesk({
+const schibstedGrotesk = Schibsted_Grotesk({
   variable: "--font-schibsted-grotesk",
   subsets: ["latin"],
 });
@@ -15,7 +16,7 @@ const martianMono = Martian_Mono({
 
 export const metadata: Metadata = {
   title: "DevEvent",
-  description: "The hub for Every Dev Event You Mustn't Miss",
+  description: "The Hub for Every Dev Event You Mustn't Miss",
 };
 
 export default function RootLayout({
@@ -26,11 +27,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${schibsteg_Grotesk.variable} ${martianMono.variable} min-h-screen antialiased`}
+        className={`${schibstedGrotesk.variable} ${martianMono.variable} min-h-screen antialiased`}
       >
+        <Navbar />
+
         <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
           <LightRays
-            raysOrigin="top-center"
+            raysOrigin="top-center-offset"
             raysColor="#5dfeca"
             raysSpeed={0.5}
             lightSpread={0.9}
@@ -41,6 +44,7 @@ export default function RootLayout({
             distortion={0.01}
           />
         </div>
+
         <main>{children}</main>
       </body>
     </html>
